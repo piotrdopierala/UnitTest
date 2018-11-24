312b4acc83a8adbd8c58ee6699fd777d5ca16047
Piotr Dopierala <ptr.dopierala@gmail.com>
2018-11-24 10:15:05 +0100

    Initial commit

---
+++ src/main/java/pl/dopierala/Calculator.java
@@ -0,0 +1,7 @@
+package pl.dopierala;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/pl/dopierala/CalculatorTest.java
@@ -0,0 +1,5 @@
+package pl.dopierala;
+
+public class CalculatorTest {
+    
+}
